edd0335150ade95ba1839a14e1eaebed28c0a4f8
Jebakumar <jebakumar.6208@gmail.com>
2025-02-13 10:34:26 +0530

    completed

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
+
+// MongoDB connection
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to database"))
+.catch(err => console.error("Error connecting to database:", err));
 
 app.use(express.static('static'));
 
